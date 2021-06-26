d1c1f2870fceb34d88b76696bb2e7d6655d6264e
nano <76194982+nano2nano@users.noreply.github.com>
2021-06-26 18:32:19 +0900

    Fix bugs.

--- cmd/valorant_tips/main.go
+++ cmd/valorant_tips/main.go
@@ -3,15 +3,10 @@ package main
 import (
 	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/nano2nano/valorant_tips/internal/route"
 )
 
 func main() {
-	if err := godotenv.Load(".env"); err != nil {
-		panic("Error loading .env file")
-	}
 	router := route.Init()
-	print(os.Getenv("PORT"))
 	router.Logger.Fatal(router.Start(":" + os.Getenv("PORT")))
 }
